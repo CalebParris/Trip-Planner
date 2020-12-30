af3149a1108a4f6ada4fc1a1bd31e9981290c163
Caleb Parris <calebparris@live.com>
2020-12-30 13:03:37 -0600

    Fixed a function name

--- assets/main.js
+++ assets/main.js
@@ -1,18 +1,13 @@
-
-
 $("#searchButton").on("click", function (event) {
   event.preventDefault();
   $(".warning").addClass("hide");
   $("#restaurants").addClass("hide");
   $("#drivingDirections").addClass("hide");
-  
 
   console.log($("#endLoc").val().trim());
   if ($("#endLoc").val().trim() !== "") {
-
     validateDestination();
-  }
-  else {
+  } else {
     toggleWarning();
     $("#end-loc-warning").removeClass("hide");
   }
@@ -20,11 +15,11 @@ $("#searchButton").on("click", function (event) {
 
 $(document).on("click", ".hotel-address", function () {
   $(".warning").addClass("hide");
-  $("#start-loc-warning").text('Please enter a valid address for directions');
-  var hotelZip = $(this).attr("data-zip")
-  $("#restaurantResults").remove()
+  $("#start-loc-warning").text("Please enter a valid address for directions");
+  var hotelZip = $(this).attr("data-zip");
+  $("#restaurantResults").remove();
 
-  restaurantSearch(hotelZip)
+  restaurantSearch(hotelZip);
   $("#restaurants").removeClass("hide");
 
   if ($("#startLoc").val().trim() !== "") {
@@ -32,30 +27,31 @@ $(document).on("click", ".hotel-address", function () {
     $("#map").css("display", "block");
     // Render Maps and catch directions, distance and time
     var origin = $("#startLoc").val();
-    var destination = $(this).text()
-    Geocode2Addresses(origin, destination)
+    var destination = $(this).text();
+    Geocode2Addresses(origin, destination);
     $("#drivingDirections").removeClass("hide");
   } else {
     toggleWarning();
     $("#start-loc-warning").removeClass("hide");
   }
-})
-
+});
 
 $("#userLocBtn").on("click", function () {
   event.preventDefault();
   getCurrentLoc();
-})
+});
 
-$("#prevent").on("click", function(){
-  event.preventDefault()
-})
-
+$("#prevent").on("click", function () {
+  event.preventDefault();
+});
 
 function toggleWarning() {
-  $([document.documentElement, document.body]).animate({
-    scrollTop: $("#search").offset().top
-  }, 500);
+  $([document.documentElement, document.body]).animate(
+    {
+      scrollTop: $("#search").offset().top,
+    },
+    500
+  );
 
   console.log("ERROR");
 }
